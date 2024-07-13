2e0907d62c357464288140edbd168000cf96f7fe
Lizernz <154061095+Lizernz@users.noreply.github.com>
2024-07-13 17:20:35 +0100

    added functions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-
+import React, { useState }from "react";
 import './App.css';
 
 function App() {
+  const [city, setCity] = useState("");
+
+function updateCity (event) {
+ setCity(event.target.value);
+};
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello world.....</h1>
+        <div class="cities">
+          <ul>
+            <li>Lisbon</li>
+            <li>Paris</li>
+            <li>Sydney</li>
+            <li>San Francisco</li>
+          </ul>
+        </div>
       </header>
+    <div>
+    
+    <form>
+<input type="search" placeholder="Enter a city"
+value={city}
+ onChange={updateCity} />
+<button type="submit" > Search </button>
+    </form>
+    </div>
     </div>
   );
 }
